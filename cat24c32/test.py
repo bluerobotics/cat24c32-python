7f0bdcdae4e9a5109c48279db6ea0dea2f40cf87
Jacob Walser <jwalser90@gmail.com>
2021-10-05 04:05:52 -0400

    test: update with llog helpers

--- cat24c32/test.py
+++ cat24c32/test.py
@@ -2,35 +2,22 @@
 
 import argparse
 from cat24c32 import CAT24C32
+from llog import LLogWriter
 from random import randint
-from pathlib import Path
-import llog
 import time
 
-device = "cat24c32"
-defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
-
-parser = argparse.ArgumentParser(description=f'{device} test')
-parser.add_argument('--output', action='store', type=str, default=None)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--frequency', action='store', type=int, default=1)
+parser = LLogWriter.create_default_parser(__file__, 'cat24c32')
 args = parser.parse_args()
 
-
-with llog.LLogWriter(args.meta, args.output) as log:
+with LLogWriter(args.meta, args.output, console=args.console) as log:
     eeprom = CAT24C32()
 
-    while True:
+    def data_getter():
         address = randint(0, 0xffe) # the last byte is reserved as a serial ID, so we don't want to touch it
         writedata = randint(0, 0xff)
-        try:
-            eeprom.write(address, [writedata])
-            readdata = eeprom.read(address)[0]
-            verified = writedata == readdata
+        eeprom.write(address, [writedata])
+        readdata = eeprom.read(address)[0]
+        verified = writedata == readdata
+        return f'{address} {writedata} {verified}'
 
-            log.log(llog.LLOG_DATA, f"{address} {writedata} {verified}")
-        except Exception as e:
-            log.log(llog.LLOG_ERROR, e)
-
-        if args.frequency:
-            time.sleep(1.0/args.frequency)
+    log.log_data_loop(data_getter, parser_args=args)
